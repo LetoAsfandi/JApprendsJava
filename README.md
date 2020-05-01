c15f731151a15f2dfe8caf7dfc4c4e19a8a61af1
Thierry Ernesto <t.baribaud@gmail.com>
2020-05-01 16:28:12 +0200

    Initialisation du projet, version 1.00,GrosPC - Création du fichier .gitignore - Création du fichier README.md - Création de la classe JApprendsJava

---
+++ README.md
@@ -0,0 +1,14 @@
+# J'apprends Java 
+
+J'ai placé ici tout ce qui me sert à apprendre Java.
+
+## Pré-requis :
+- JDK Oracle 11
+- Intelliji 2020 Community Edition
+- GitHub
+- GDrive
+- Internet
+- Git 2.26.2 pour Windows 
+
+## Références :
+- [Apprenez à programmer en Java](https://openclassrooms.com/fr/courses/26832-apprenez-a-programmer-en-java)
---
+++ src/com/anstel/japprendsjava/JApprendsJava.java
@@ -0,0 +1,15 @@
+/*
+ * Ce programme contient tout ce qui me sert à apprendre java
+ * @version 1.00
+ * @author Thierry Ernesto Baribaud.
+ */
+package com.anstel.japprendsjava;
+
+/**
+ * classe décrivant le programme JApprendsJava
+ */
+public class JApprendsJava {
+    public static void main(String... args) {
+        System.out.println("J'apprends Java!");
+    }
+}
